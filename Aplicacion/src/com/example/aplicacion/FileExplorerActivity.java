50bb5da96a0ee21ac3ba62a21f0e65aa77fab976
NikNitro <adrimlg92@gmail.com>
2014-11-16 12:34:52 +0100

    Cambios hechos por cgg para que lea y use los radiobuttons... Algo falta...

--- Aplicacion/src/com/example/aplicacion/FileExplorerActivity.java
+++ Aplicacion/src/com/example/aplicacion/FileExplorerActivity.java
@@ -3,7 +3,6 @@ package com.example.aplicacion;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.DialogInterface;
@@ -17,107 +16,106 @@ import android.widget.Toast;
 
 public class FileExplorerActivity extends ListActivity {
 
-   private List<String> item = null;
-   private List<String> path = null;
-   private String root=Selector_memoria.ruta;
-   private TextView myPath;
-   private String archivo_seleccionado;
-   private String archivo_nombre;
+	private List<String> item = null;
+	   private List<String> path = null;
+	   private String root=Selector_memoria.ruta;
+	   private TextView myPath;
+	   private String archivo_seleccionado;
+	   private String archivo_nombre;
 
-   public void finish() {
-       Intent data = new Intent();
-       data.putExtra("archivo_seleccionado", archivo_seleccionado);
-       data.putExtra("archivo_nombre", archivo_nombre);
-       setResult(RESULT_OK, data);
-       super.finish();
-   }
+	   public void finish() {
+	       Intent data = new Intent();
+	       data.putExtra("archivo_seleccionado", archivo_seleccionado);
+	       data.putExtra("archivo_nombre", archivo_nombre);
+	       setResult(RESULT_OK, data);
+	       super.finish();
+	   }
 
-   @Override
-   public void onCreate(Bundle savedInstanceState) {
-          super.onCreate(savedInstanceState);
-          setContentView(R.layout.main_file);          
-          myPath = (TextView)findViewById(R.id.path);
-          
-          //Por si no existe tarjeta SD que no explote.
-          try {
-        	  getDir(root);
-          } catch(NullPointerException e) {
-        	  Toast texto1 = Toast.makeText(getBaseContext(), "No hay SD", Toast.LENGTH_SHORT);
-        	  texto1.show();
-          }
-   }
+	   @Override
+	   public void onCreate(Bundle savedInstanceState) {
+	          super.onCreate(savedInstanceState);
+	          setContentView(R.layout.main_file);          
+	          myPath = (TextView)findViewById(R.id.path);
+	          getDir(root);
+	          try {
+	        	  getDir(root);
+	          } catch(NullPointerException e) {
+	        	  Toast texto1 = Toast.makeText(getBaseContext(), "No hay SD", Toast.LENGTH_SHORT);
+	        	  texto1.show();
+	          }
+	   }
 
-   private void getDir(String dirPath){
-       myPath.setText(dirPath);
-       item = new ArrayList<String>();
-       path = new ArrayList<String>();
-       File f = new File(dirPath);
-       File[] files = f.listFiles();
-       
-       if(!dirPath.equals(root)){
-         item.add(root);
-         path.add(root);
-         item.add("../");
-         path.add(f.getParent());
-       }
+	   private void getDir(String dirPath){
+	       myPath.setText(dirPath);
+	       item = new ArrayList<String>();
+	       path = new ArrayList<String>();
+	       File f = new File(dirPath);
+	       File[] files = f.listFiles();
+	       
+	       if(!dirPath.equals(root)){
+	         item.add(root);
+	         path.add(root);
+	         item.add("../");
+	         path.add(f.getParent());
+	       }
 
-       for(int i=0; i < files.length; i++){
-         File file = files[i];
-         path.add(file.getPath());
-         if(file.isDirectory())
-          item.add(file.getName() + "/");
-         else
-          item.add(file.getName());
-       }
+	       for(int i=0; i < files.length; i++){
+	         File file = files[i];
+	         path.add(file.getPath());
+	         if(file.isDirectory())
+	          item.add(file.getName() + "/");
+	         else
+	          item.add(file.getName());
+	       }
 
-       ArrayAdapter<String> fileList =
-          new ArrayAdapter<String>(this, R.layout.row_file, item);
+	       ArrayAdapter<String> fileList =
+	          new ArrayAdapter<String>(this, R.layout.row_file, item);
 
-       setListAdapter(fileList);
+	       setListAdapter(fileList);
 
-   }
+	   }
 
-   @Override
-   protected void onListItemClick(ListView l, View v, int position, long id) {
-     
-    File file = new File(path.get(position));
-    if (file.isDirectory())
-    {
-    if(file.canRead())
-       getDir(path.get(position));
-      else
-      {
-      new AlertDialog.Builder(this)
-      .setTitle("[" + file.getName() + "] folder no puede ser leido!")
-      .setPositiveButton("OK", 
-      new DialogInterface.OnClickListener() {
-         @Override
-         public void onClick(DialogInterface dialog, int which) {}
-        }).show();
-     }
-    }
-    else
-    {
-      archivo_seleccionado = file.getAbsolutePath();
-      archivo_nombre = file.getName();
-      MainActivity.ruta.setText(archivo_seleccionado);
-      finish();
-    }
-  }
-   
-   @Override
-   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-     if (resultCode == RESULT_OK && requestCode == 555) {
-       if (data.hasExtra("archivo_seleccionado")) {
-         archivo_seleccionado = data.getExtras().getString("archivo_seleccionado");
-         archivo_nombre = data.getExtras().getString("archivo_nombre");
-       //MainActivity.ruta.setText(archivo_nombre);				//	<--- archivo not in scope
-        // MainActivity.ruta = (TextView)archivo_nombre;
-        // MainActivity.ruta = (TextView)findViewById(R.id.textView1);
+	   @Override
+	   protected void onListItemClick(ListView l, View v, int position, long id) {
+	     
+	    File file = new File(path.get(position));
+	    if (file.isDirectory())
+	    {
+	    if(file.canRead())
+	       getDir(path.get(position));
+	      else
+	      {
+	      new AlertDialog.Builder(this)
+	      .setTitle("[" + file.getName() + "] folder no puede ser leido!")
+	      .setPositiveButton("OK", 
+	      new DialogInterface.OnClickListener() {
+	         @Override
+	         public void onClick(DialogInterface dialog, int which) {}
+	        }).show();
+	     }
+	    }
+	    else
+	    {
+	      archivo_seleccionado = file.getAbsolutePath();
+	      archivo_nombre = file.getName();
+	      MainActivity.ruta.setText(archivo_seleccionado);
+	      finish();
+	    }
+	  }
+	   
+	   @Override
+	   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	     if (resultCode == RESULT_OK && requestCode == 555) {
+	       if (data.hasExtra("archivo_seleccionado")) {
+	         archivo_seleccionado = data.getExtras().getString("archivo_seleccionado");
+	         archivo_nombre = data.getExtras().getString("archivo_nombre");
+	       //MainActivity.ruta.setText(archivo_nombre);				//	<--- archivo not in scope
+	        // MainActivity.ruta = (TextView)archivo_nombre;
+	        // MainActivity.ruta = (TextView)findViewById(R.id.textView1);
 
-       }
-     }
-   }
-   
+	       }
+	     }
+	   }
+	   
 
 }
--- Aplicacion/src/com/example/aplicacion/MainActivity.java
+++ Aplicacion/src/com/example/aplicacion/MainActivity.java
@@ -12,12 +12,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,21 +29,27 @@ public class MainActivity extends Activity {
 	private Button open_file_explorer;
 	private Button comprimir;
 	private Button descomprimir;
-	private Button abrir;
 	public static TextView ruta;
 	private String std;
 	public static  String prueba;
 	public static TextView informacion;
-	
+	private Button abrir;
+	private RadioGroup rg;
+	 private RadioButton rb;
+
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	
+		
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
               
         ruta = (TextView)findViewById(R.id.txtNombreArchivo);
         informacion = (TextView)findViewById(R.id.textView2);
+        informacion.setMovementMethod(new ScrollingMovementMethod());
+        
+        rg = (RadioGroup) findViewById(R.id.rdgGrupo);
+        
         
         comprimir = (Button)findViewById(R.id.btnComprimir);
         comprimir.setOnClickListener(new OnClickListener() {
@@ -49,8 +58,19 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 			
 				std = ruta.getText().toString();
-				informacion.setText("Información: " +std);
-				new Comprimir(std, 0, 1);
+
+			
+
+				if(std.equals(getResources().getString(R.string.rutaDelArchivo))){
+					Toast  texto1 = Toast.makeText(getBaseContext(), "Seleccione archivo", Toast.LENGTH_SHORT);
+		        	texto1.show();
+	        	}else{
+					informacion.setText("Información: " +std);
+					int seleccionado = rg.getCheckedRadioButtonId();
+					rb = (RadioButton) findViewById(seleccionado);
+					new Comprimir(std, 0, 1, rb.getText().toString());
+	        	}
+
 			}
 		});
         
@@ -61,16 +81,28 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 			
 				std = ruta.getText().toString();
-				informacion.setText("Información: " +std);
-				new Comprimir(std, 0, 0);
+				if(std.equals(getResources().getString(R.string.rutaDelArchivo))){
+					Toast  texto1 = Toast.makeText(getBaseContext(), "Seleccione archivo", Toast.LENGTH_SHORT);
+		        	texto1.show();
+	        	}else{
+					informacion.setText("Información: " +std);
+					int seleccionado = rg.getCheckedRadioButtonId();
+					rb = (RadioButton) findViewById(seleccionado);
+					new Comprimir(std, 0, 0, rb.getText().toString());
+	        	}
+
 			}
 		});
         
         open_file_explorer = (Button)findViewById(R.id.btnBuscar);
         open_file_explorer.setOnClickListener(new OnClickListener() {      
           public void onClick(View v) {
-            Intent file_explorer = new Intent(MainActivity.this,Selector_memoria.class);
+            
+        	  
+        	  Intent file_explorer = new Intent(MainActivity.this,Selector_memoria.class);
+        	 
             startActivityForResult(file_explorer, 555);// <-- ¿?
+       
           }
           
         });
@@ -83,29 +115,30 @@ public class MainActivity extends Activity {
 				// Este boton mostrará el archivo.
 	        	std = ruta.getText().toString();
 	        	
+	        	if(std.equals(getResources().getString(R.string.rutaDelArchivo))){
+	        		Toast  texto1 = Toast.makeText(getBaseContext(), "Seleccione archivo", Toast.LENGTH_SHORT);
+		        	texto1.show();
+	        	}else{
+		        	try {
+						BinaryIn In = new BinaryIn(std);
+						informacion.setText(In.readString());
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+						Log.i("error", e.getMessage());
+					}	
+	        	}
+
 	        	
-	        	
-	        	///Para de mientras
-				Toast texto1 = Toast.makeText(getBaseContext(), "Aún no implementado.", Toast.LENGTH_SHORT);
-	        	texto1.show();
+
+
 	        	
 				
 			}
 		});
     }
-    public boolean existsFile(String fileName) {
-    	Log.i("metodo", "exits");
-    	if(fileList().length == 0){
-    		Log.i("metodo", "vacio");
-    	}
-        for (String tmp : fileList()) {
-        	Log.i("fichero1", tmp);
-            if (tmp.equals(fileName))
-                return true;
-        }
-        return false;
-    }
-
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
